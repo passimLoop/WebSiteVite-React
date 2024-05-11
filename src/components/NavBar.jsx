112231ebf5908724a9037cf76f1c3c7c60b59b7c
pamonge <pablomonge55@gmail.com>
2024-05-11 15:28:31 -0300

    Fix: styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,6 @@ import style from "./NavBarStyles.module.css"
 
 const NavBar = () => {
 
-	const [click, setClick] = useState(false);
-	const handleClick = () => setClick(!click);
-
 	const [color, setColor] = useState(false);
 	const changeColor = () => {
 		if (window.scrollY >= 100) {
@@ -17,14 +14,17 @@ const NavBar = () => {
 		}
 	};
 
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+
 	window.addEventListener('scroll', changeColor);
 
   	return (
-    	<div className={color? `{${style.header} ${style.headerbg}}` : `${style.header}`}>
+    	<div className={color ? (`${style.header} ${style.headerbg}`) : (`${style.header}`) }>
         	<Link to="/" >
         	    <h1>PassimLoop</h1>  {/* Aca pense en poner el logo más chico en vez del nombre escrito, pero me parece que hay que cambiar algo de los 3 lugares donde aparece el nombre, es muy redundante */}
         	</Link>
-			<ul className={click? `{${style.navmenu} ${style.active}}` : `${style.navmenu}`}>
+			<ul className={click ? `${style.navMenuActive}` : `${style.navmenu}`}>
 				<li><Link to="/">Inicio</Link></li>
 				<li><Link to="/about">Acerca de</Link></li>
 				<li><Link to="/products">Productos</Link></li>
