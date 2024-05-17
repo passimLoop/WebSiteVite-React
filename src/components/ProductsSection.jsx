a8f46f09f06fe82c137e8fceaac161cc6c23ef43
pamonge <pablomonge55@gmail.com>
2024-05-16 22:31:42 -0300

    Refactor: ProductSectionStyles

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductsSectionStyles.module.css";
 import academia from "../assets/img/academia/Captura desde 2024-04-20 03-07-21.png";
 import sitio from "../assets/img/sitio/Captura desde 2024-04-20 03-10-43.png";
-import tienda from "../assets/img/tienda/Captura desde 2024-04-22 19-16-08.png";
 
 const ProductsSection = () => {
+
+	const [ classImg1, setClassImg1 ] = useState (styles.img1);
+	const [ classImg2, setClassImg2 ] = useState (styles.img2);
+
+	const handleClick = () => {
+		setClassImg1((prevClass) => ( prevClass === styles.img1 ? styles.img2 : styles.img1 ));
+		setClassImg2((prevClass) => ( prevClass === styles.img2 ? styles.img1 : styles.img2 ));
+	}
+
   return (
 		<div className={styles.container}>
 			<h2>Nuestros Proyectos</h2>
@@ -23,11 +31,10 @@ const ProductsSection = () => {
 						<p>Alguna otra cosa mas de chamullo</p>
 					</div>
 				</div>
-				<div className={styles.imgContainer}>
-						<img className={styles.img1} src={academia} alt="Vista sitio academia" />
-						<img className={styles.img2} src={sitio} alt="Vista de sitio ByteCrafters" />
-						<img className={styles.img3} src={tienda} alt="Vista de sitio tienda" />
-					</div>
+				<div onClick={handleClick} className={styles.imgContainer}>
+					<img className={classImg1} src={academia} alt="Vista sitio academia" />
+					<img className={classImg2} src={sitio} alt="Vista de sitio ByteCrafters" />
+				</div>
 			</div>
 		</div>
 	)
