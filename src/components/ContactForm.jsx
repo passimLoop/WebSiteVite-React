825ce047a4a5fa263ccf129564ed4705d66dd99b
pamonge <pablomonge55@gmail.com>
2024-05-21 23:39:12 -0300

    Delete: modals Refactos: validacion form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,14 +1,62 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Modal from './Modal';
 import emailjs from '@emailjs/browser';
 import style from './ContactFormStyles.module.css';
 
 const ContactForm = () => {
 
+  const [ formData, setFormData ] = useState({
+    user_name: '',
+    user_email: '',
+    subject: '',
+    message: ''
+  });
+
+  const [ errors, setErrors ] = useState({});
+  const [ isValid, setIsValid ] = useState(false);
+  
   const [ mailSend, setMailSend ] = useState(false);
   const [ mailFailed, setMailFailed ] = useState(false);
 
   const form = useRef();
+
+  const validate = () => {
+    const newErrors = {};
+
+    if(!formData.user_name) {
+      newErrors.user_name = 'Nombre y apellido es requerido.';
+    }
+
+    if (!formData.user_email) {
+      newErrors.user_email = 'Email es requerido.';
+    } else if (!/\S+@\S+\.\S+/.test(formData.user_email)) {
+      newErrors.user_email = 'Email invalido.';
+    }
+
+    if (!formData.subject) {
+      newErrors.subject = 'Asunto es requerido.';
+    }
+
+    if (!formData.message) {
+      newErrors.message = 'Mensaje es requerido.';
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  useEffect(() => {
+    setIsValid(validate());
+  }, [formData]);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -34,19 +82,59 @@ const ContactForm = () => {
         <h2>Contacto</h2>
         <form className={style.form} ref={form} onSubmit={sendEmail}> 
           <label htmlFor='user_name'>Nombre y apellido</label>
-          <input id='user_name' type="text" name="user_name" autoComplete='off'/>
+          <input 
+            id='user_name' 
+            type="text" 
+            name="user_name"
+            value={formData.user_name}
+            onChange={handleChange} 
+            autoComplete='off'/>
+            {errors.user_name && 
+              <span className={style.error}>{errors.user_name}</span>
+            }
+          
           <label htmlFor='user_email'>Email</label>
-          <input id='user_email' type="email" name="user_email" autoComplete='off'/>
+          <input 
+            id='user_email' 
+            type="email" 
+            name="user_email" 
+            value={formData.user_email}
+            onChange={handleChange}
+            autoComplete='off'/>
+            {errors.user_email && 
+              <span className={style.error}>{errors.user_email}</span>
+            }
+
           <label htmlFor='subject'>Asunto</label>
-          <input id='subject' type="text" name="subject" autoComplete='off'/>
+          <input 
+            id='subject' 
+            type="text" 
+            name="subject" 
+            value={formData.subject}
+            onChange={handleChange}
+            autoComplete='off'/>
+            {errors.subject &&
+              <span className={style.error}>{errors.subject}</span>
+            }
+
           <label htmlFor='message'>Mensaje</label>
-          <textarea id='message' name="message" autoComplete='off'/>
-          <input type="submit" value="Enviar" />
+          <textarea 
+            id='message' 
+            name="message" 
+            value={formData.message}
+            onChange={handleChange}
+            autoComplete='off'/>
+            {errors.message && 
+              <span className={style.error}>{errors.message}</span>
+            }
+
+          <input type="submit" value="Enviar" disabled={!isValid} />
         </form>
+
         {mailSend && (
         <Modal isOpen={mailSend} onClose={() => setMailSend(false)}>
           <h4>AVISO</h4>
-          <p>Mensaje enviado éxitosamente!!!</p>
+          <p>Mensaje enviado exitosamente!!!</p>
           <p>Sera contactado a la brevedad por nuestro personal</p>
         </Modal>
         )}
